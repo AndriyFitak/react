07eaf9186878f1b56f8bf82443f7598e16d567d5
AndriyFitak <116104837+AndriyFitak@users.noreply.github.com>
2023-10-07 12:51:39 +0200

    js

---
+++ client/src/Components/App.jsx
@@ -0,0 +1,13 @@
+import Button from "./Button/Button"
+
+const App = () => {
+    return (
+        <div>
+        <Button text="Sign in"/>
+        <h1>Test</h1>
+        <Button  text="Sign up"/>
+        
+        </div>
+    )
+}
+export default App
