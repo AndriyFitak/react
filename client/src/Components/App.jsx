a884fb592bb6667986f68df9f17b039360603ff4
AndriyFitak <116104837+AndriyFitak@users.noreply.github.com>
2023-11-21 18:19:57 +0200

    css-piling

--- client/src/Components/App.jsx
+++ client/src/Components/App.jsx
@@ -1,10 +1,29 @@
+import React from 'react';
+import './MyComponent.css';
+import Burger from "../img/burger.png"
+import Chebyrek from "../img/chebyrek.jpg"
+import Chiken from "../img/chiken.jpg"
+import Potato from "../img/potatoes.jpg"
 
-import ProductList from "./ProductsList/ProductList"
-const App = () => {
-    return (
-        <>  
-        <ProductList/>
-        </>
-    )
-}
-export default App
+
+
+const MyComponent = (i) => {
+  return (
+    <div>
+    <div className="image-container">
+      <img className={"image"} src={Burger} alt=""/>
+      <img className={"image"} src={Chebyrek} alt=""/>
+      <img className={"image"} src={Chiken} alt=""/>
+      <img className={"image"} src={Potato} alt=""/>
+    </div>
+    <div className="text-names">
+        <p className="image-text">Smoked salmon burger</p>
+        <p className="image-text">Chebyrek nyam nyam</p>
+        <p className="chiken-text image-text">Chicken Biryani Gout</p>
+        <p className="image-text">Spicy potato</p>
+    </div>
+    </div>
+  );
+};
+
+export default MyComponent;
